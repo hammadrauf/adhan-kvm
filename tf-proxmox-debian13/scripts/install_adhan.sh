696d21b8180c6a4880aeed59199df0f896746bec
Hammad Rauf <rauf.hammad@gmail.com>
2026-02-19 20:51:01 -0500

    wip

--- tf-proxmox-debian13/scripts/install_adhan.sh
+++ tf-proxmox-debian13/scripts/install_adhan.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 SUPERUSERNAME=$1
-sudo apt install -y git
+sudo apt update -y && sudo apt upgrade -y && sudo apt install -y git
 echo 'Running custom script...'
 cd /home/${SUPERUSERNAME}
 git clone https://github.com/hammadrauf/adhan-tools.git
 cd adhan-tools
 chmod +x *.sh
 sudo ./install.sh
+cd /home/${SUPERUSERNAME}
 echo 'Custom script execution completed....'
